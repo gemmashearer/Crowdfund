89306320096371f598687bb8f8b6a049dd4318cc
Gemma Shearer <gemmaandjude@gmail.com>
2015-02-02 14:26:11 +0000

    crowdfund charity app

---
+++ crowdfund.rb
@@ -0,0 +1,9 @@
+project1 ='ABC'
+funding1 = 1000
+puts "Project #{project1} has $#{funding1} in funding."
+
+project2 ="LMN"
+project3 ="XYZ"
+
+puts"Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
+
